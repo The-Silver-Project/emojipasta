efac963ccd9c36307c77534fd45655c88140ea3d
Lakshya Gupta <lakshya@thesilverlabs.com>
2021-07-10 17:04:41 +0530

    Add emoji preview

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import bake from './bakery/bake';
+import mapping from './mapping.json';
 
 const result = document.getElementById('result');
 const fg = document.getElementById('fg');
@@ -6,9 +7,23 @@ const bg = document.getElementById('bg');
 const text = document.getElementById('text');
 const swap = document.getElementById('swap');
 
+function replaceStr(str, rep) {
+  if (!rep.startsWith(':') || !rep.endsWith(':')) return str;
+  const key = rep.slice(1, -1);
+  if (!mapping[key]) return str;
+  return str.replaceAll(rep, `&#x${mapping[key]}`);
+}
+
+function getChangedStr(str, bgVal, fgVal) {
+  return replaceStr(replaceStr(str, fgVal), bgVal);
+}
+
 function run() {
   const str = bake(text.value, { fg: fg.value, bg: bg.value });
-  result.innerText = str;
+
+  const changedStr = getChangedStr(str, bg.value, fg.value);
+  result.innerHTML = changedStr;
+
   navigator.clipboard.writeText(str);
 }
 
